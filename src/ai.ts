0389d8b6526a59f3daa0cca52fd04977c38e5ee1
sanicboi <alexandrzezekalo@gmail.com>
2025-05-28 07:55:35 +0200

    crm

--- src/ai.ts
+++ src/ai.ts
@@ -3,6 +3,36 @@ import fs from "fs";
 import path from "path";
 
 export class AI {
+
+  private tools: OpenAI.Responses.Tool[] = [
+    {
+      type: 'function',
+      name: 'saveUser',
+      strict: true,
+      description: 'сохраняет пользователя в базе данных для дальнейшей работы с ним. Обязательно вызывай ее после окончания диалога',
+      parameters: {
+        type: "object",
+        properties: {
+          comment: {
+            type: "string",
+            description: "Комментарий про диалог с пользователем. Постарайся довольно подробно описать его и дать советы по работе с этим клиентом"
+          },
+          class: {
+            type: "string",
+            enum: [
+              "A", "B", "C", "D"
+            ],
+            description: "Тип пользователя. A - Заполнил анкету или заявку. B - Оставил номер телефона или потребовал менеджера. C - перешел в чат-бота или ты ему скинул на него ссылку. D - прочие случаи"
+          },
+          required: [
+            "class", "comment"
+          ],
+          additionalProperties: false
+        }
+      }
+    }
+  ]
+
   private openai = new OpenAI({
     apiKey: process.env.OPENAI_TOKEN,
   });
@@ -59,13 +89,42 @@ export class AI {
     text: string;
     id: string;
   }> {
-    const res = await this.openai.responses.create({
+    let res = await this.openai.responses.create({
       input: msg,
       model: "gpt-4.1",
       store: true,
       previous_response_id: prev,
-      temperature: 1
+      temperature: 1,
+      tools: this.tools
     });
+
+    for (const out of res.output) {
+      if (out.type === 'function_call') {
+        const args: {
+          comment: string,
+          class: "A" | "B" | "C" | "D"
+        } = JSON.parse(out.arguments);
+
+
+        
+
+
+        res = await this.openai.responses.create({
+          previous_response_id: res.id,
+          input: [
+            {
+              type: 'function_call_output',
+              call_id: out.call_id,
+              output: "ПОльзователь сохранен. Не говори ему ничего про функцию."
+            }
+          ],
+          model: "gpt-4.1",
+          store: true,
+          temperature: 0.5
+        });
+        break;
+      }
+    }
 
     return {
       text: res.output_text,
