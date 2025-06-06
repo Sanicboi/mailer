13121cc1bac13e9636861d63dc254c3de4827b85
sanicboi <alexandrzezekalo@gmail.com>
2025-06-06 18:14:18 +0200

    docker

--- src/data-source.ts
+++ src/data-source.ts
@@ -5,8 +5,8 @@ import { Bot } from "./entity/Bot";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
-  port: 5439,
+  host: "postgres",
+  port: 5432,
   username: "test",
   password: "test",
   database: "test",
